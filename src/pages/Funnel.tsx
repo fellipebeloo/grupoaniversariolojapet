5e62b3dace535fbc3689c51e1ec77522472e7b69
[dyad] <fellipebelo@gmail.com>
2025-10-10 17:31:05 -0300

    [dyad] Fixing chat header position on keyboard open - wrote 1 file(s)

--- src/pages/Funnel.tsx
+++ src/pages/Funnel.tsx
@@ -128,7 +128,7 @@ const FunnelPage = () => {
   }, [step, userData.name]);
 
   return (
-    <div className="h-dvh flex flex-col bg-[#0f1418] w-full overflow-hidden">
+    <div className="fixed inset-0 flex flex-col bg-[#0f1418] w-full">
       <ChatHeader />
       
       <div ref={scrollContainerRef} className="flex-1 overflow-y-auto p-4 space-y-4">
