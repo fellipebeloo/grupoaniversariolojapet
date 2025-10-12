e99b80c93bb9851a833fe348bff3ae398fd3789a
[dyad] <fellipebelo@gmail.com>
2025-10-11 23:39:11 -0300

    [dyad] Adicionei um botão para retornar à conversa com Alessandra no chat do grupo. - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/GroupChatView.tsx
+++ src/components/GroupChatView.tsx
@@ -2,6 +2,7 @@ import { GroupChatHeader } from './GroupChatHeader';
 import { GroupInfoHeader } from './GroupInfoHeader';
 import { GrupoWhatsApp } from './GrupoWhatsApp';
 import { StaticChatInput } from './StaticChatInput';
+import { Button } from '@/components/ui/button'; // Importando o componente Button
 
 interface GroupChatViewProps {
   onBack: () => void;
@@ -17,8 +18,14 @@ export const GroupChatView = ({ onBack }: GroupChatViewProps) => {
           <GrupoWhatsApp />
         </div>
       </div>
-      <div className="p-4 bg-[#202c33] border-t border-gray-700">
+      <div className="p-4 bg-[#202c33] border-t border-gray-700 flex flex-col gap-3"> {/* Adicionado flex-col e gap-3 */}
         <StaticChatInput />
+        <Button 
+          onClick={onBack} 
+          className="w-full bg-[#00a884] hover:bg-[#008f6f] text-white font-bold py-2"
+        >
+          Voltar para a conversa com Alessandra
+        </Button>
       </div>
     </div>
   );
