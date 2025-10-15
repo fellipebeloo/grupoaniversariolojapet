e7123149bcd6830d05f44dd63f2932637d683389
[dyad] <fellipebelo@gmail.com>
2025-10-15 09:42:35 -0300

    [dyad] Removendo a página inicial e definindo a página do funil como padrão - wrote 1 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import FunnelPage from "./pages/Funnel";
 import GroupTestPage from "./pages/GroupTestPage";
@@ -18,7 +17,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<FunnelPage />} /> {/* FunnelPage agora é a rota padrão */}
           <Route path="/funil" element={<FunnelPage />} />
           <Route path="/grupo-teste" element={<GroupTestPage />} />
           <Route path="/pitch-teste" element={<PitchTestPage />} />
