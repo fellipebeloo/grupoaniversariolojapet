6aaeba1a9032d2f6e9459e762acd4c31965b93bd
[dyad] <fellipebelo@gmail.com>
2025-10-20 16:58:09 -0300

    [dyad] Fixing chat sound effects - wrote 3 file(s) + extra files edited outside of Dyad

--- src/constants/audioPaths.ts
+++ src/constants/audioPaths.ts
@@ -1,5 +1,3 @@
 export const AudioPaths = {
-  messageSent: '/message-sent.mp3', // Efeito sonoro (mp3)
-  messageReceived: '/message-received.mp3', // Efeito sonoro (mp3)
   introAffiliate: '/diego.mp3', // Audio de instrução
 };
